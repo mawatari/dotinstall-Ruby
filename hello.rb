ddb37d6aa67d998d3ba8b100d631c9c4833c34d7
Naoto Mawatari <github.mawatari@gmail.com>
2015-02-22 06:59:39 +0900

    #12 %記法を使ってみよう

--- hello.rb
+++ hello.rb
@@ -1,11 +1,18 @@
-# 型変換
-a = 10
-b = "5"
-# p a + b # => TypeError
-p a + b.to_i # => 15
-p a + b.to_f # => 15.0
-p a.to_s + b # => "105"
-p (a.to_s + b).to_i # => 105
+# %記法
+s = "you say \"hello\""
+s = %Q(you say "hello")
+puts s # => you say "hello"
+s = %(you say "hello")
+puts s # => you say "hello"
 
-h = {mawatari: 100, tanaka: 200}
-p h.to_a # => [[:mawatari, 100], [:tanaka, 200]]
+s = 'Let\'s go'
+s = %q(Let's go)
+puts s # => Let's go
+
+a = ["a", "b", "c"]
+a = %W(a b c)
+p a # => ["a", "b", "c"]
+
+a = ['a', 'b', 'c']
+a = %w(a b c)
+p a # => ["a", "b", "c"]

b5a89a454381249d8e18aea3a1c52781e96284cd
Naoto Mawatari <github.mawatari@gmail.com>
2015-02-22 06:14:20 +0900

    #10 ハッシュオブジェクトを使おう

--- hello.rb
+++ hello.rb
@@ -1,48 +1,17 @@
-# 配列オブジェクト
-# Array Class
-sales = [5, 8, 4]
-puts sales[0] # => 5
-sales[0] = 10
-puts sales[0] # => 10
+# ハッシュ
+# Hash Class
+sales = {"mawatari" => 200, "tanaka" => 100}
+p sales["mawatari"] # => 200
 
-# 範囲指定
-p sales[0..2] # => [10, 8, 4]
-p sales[0...2] # => [10, 8]
-p sales[-1] # => 4
-p sales[1, 2] # [8, 4]
+# Symbol
+sales = {:mawatari => 200, :tanaka => 100}
+p sales[:mawatari] # => 200
 
-# 範囲を書き換え
-sales[0...2] = [1, 6]
-p sales # => [1, 6, 4]
+sales = {mawatari: 200, tanaka: 100}
+p sales[:mawatari] # => 200
 
-# 要素の途中から要素を追加
-sales = [5, 8, 4]
-sales[1, 0] = [10, 11, 12]
-p sales # => [5, 10, 11, 12, 8, 4]
-
-# 指定範囲を空にする
-sales = [5, 8, 4]
-sales[0, 2] = []
-p sales # => [4]
-
-# method
-sales = [5, 8, 4]
-
-# 要素数
-p sales.size # => 3
-
-# 昇順ソート
-p sales.sort # => [4, 5, 8]
-
-# 降順ソート
-p sales.sort.reverse # => [8, 5, 4]
-
-# 末尾に要素を追加
-p sales.push 100 # => [5, 8, 4, 100]
-
-sales = [5, 8, 4]
-p sales << 100 # => [5, 8, 4, 100]
-
-sales = [5, 8, 4]
-p sales << 100 << 105 # => [5, 8, 4, 100, 105]
-
+# methods
+p sales.size # => 2
+p sales.keys # => [:mawatari, :tanaka]
+p sales.values # => [200, 100]
+p sales.has_key? :mawatari # => true

646baa9a229c8eb90acb631db3a6067a0a926511
Naoto Mawatari <github.mawatari@gmail.com>
2015-02-23 05:20:16 +0900

    #14 真偽値と条件演算子

--- hello.rb
+++ hello.rb
@@ -1,4 +1,8 @@
-# 条件分岐
+# 真偽値と条件演算子
+=begin
+true: それ以外(0や''を含む)
+false: false nil(オブジェクトが存在しない)
+=end
 score = 80
 if score > 60
   puts "OK!"
@@ -10,3 +14,12 @@ end
 
 # 後置法
 puts "OK!" if score > 60
+
+# 条件演算子
+a = 10
+b = 20
+c = a > b ? a : b
+puts c # => 20
+
+# 多重代入
+a, b = 10, 20

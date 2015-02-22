ecd01f77694c21bfca91fa2d9dc23e4fba82f32c
Naoto Mawatari <github.mawatari@gmail.com>
2015-02-23 05:33:52 +0900

    #15 caseで条件分岐をしてみよう

--- hello.rb
+++ hello.rb
@@ -1,25 +1,12 @@
-# 真偽値と条件演算子
-=begin
-true: それ以外(0や''を含む)
-false: false nil(オブジェクトが存在しない)
-=end
-score = 80
-if score > 60
-  puts "OK!"
-elsif score > 40
-  puts "so so.."
-else
-  puts "NG!!"
+# caseによる条件分岐
+signal = "red"
+case signal
+  when "red"
+    puts "STOP!"
+  when "green", "blue"
+    puts "GO!"
+  when "yellow"
+    puts "CAUTION!"
+  else
+    puts "wrong signal"
 end
-
-# 後置法
-puts "OK!" if score > 60
-
-# 条件演算子
-a = 10
-b = 20
-c = a > b ? a : b
-puts c # => 20
-
-# 多重代入
-a, b = 10, 20

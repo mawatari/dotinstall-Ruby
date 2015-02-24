a862db9ff6c53ad4c775794441091804122b96ff
Naoto Mawatari <github.mawatari@gmail.com>
2015-02-25 04:16:29 +0900

    #20 クラスメソッド､クラス変数を使おう

--- hello.rb
+++ hello.rb
@@ -1,15 +1,29 @@
 # クラス
 class User
+
+  # クラス変数
+  @@count = 0
+
   def initialize name
+    # インスタンス変数
     @name = name
+    @@count += 1
   end
 
+  # インスタンスメソッド
   def sayHi
     puts "Hello, my name is #{@name}."
   end
+
+  # クラスメソッド
+  def User.sayHello
+    puts "Hello from User class (#{@@count} users)"
+  end
 end
+
+User.sayHello
 
 tom = User.new "Tom"
 bob = User.new "Bob"
-tom.sayHi
-bob.sayHi
+
+User.sayHello
